f00ee1fc70ce2ba1ac7e70f27664cb551bc4264b
Simon Weber <sweb090@gmail.com>
2012-12-26 21:33:27 -0500

    add examine_keys

---
+++ examine_keys.py
@@ -0,0 +1,24 @@
+email = 'something@gmail.com'
+passwd = 'password'
+
+new_keys = ('albumMatchedId', 'pending')
+
+##
+
+from gmusicapi.api import Api
+from collections import Counter
+
+api = Api()
+api.login(email, passwd)
+
+lib = api.get_all_songs()
+print "%s total songs" % len(lib)
+
+for new_key in new_keys:
+    matching = [s for s in lib if s.get(new_key)]
+    print
+    print new_key
+    print "  %s matches" % len(matching)
+    if len(matching) > 0:
+        for key, occurs in Counter(s[new_key] for s in matching).items():
+            print "   %s: %r" % (occurs, key)
